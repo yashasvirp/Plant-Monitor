658f55242e5b2b97a5824b57a2fc9fd78f9020d1
Yashasvi R P <yashasvirp@gmail.com>
2023-02-06 13:25:31 +0530

    Extended the pin numbers from 1-4 to 1-8

--- code/arm/WorkingClient.py
+++ code/arm/WorkingClient.py
@@ -40,7 +40,7 @@ while True:
 		print("Pin 4 servo: " + makeRequest(f"get_pos?pin=4"))
 	elif key ==  ord("s"):
 		k = cv.waitKey(0)
-		if k in [ord("1"), ord("2"), ord("3"), ord("4")]:
+		if k in [ord("1"), ord("2"), ord("3"), ord("4"), ord("5"), ord("6"), ord("7"), ord("8")]:
 			pin = k - ord("0")
 
 	if key == ord("q"):
